557ace23a7d56c89d5721791b9bc51ffa21b6301
James Sturtevant <jstur@microsoft.com>
2024-10-04 20:50:06 +0000

    Update wasi internals to 0.2.1
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- src/libraries/System.Net.Http/src/System/Net/Http/WasiHttpHandler/generate-wasi-http-bindings.sh
+++ src/libraries/System.Net.Http/src/System/Net/Http/WasiHttpHandler/generate-wasi-http-bindings.sh
@@ -11,9 +11,11 @@ set -ex
 #   [cargo](https://rustup.rs/)
 #   [curl](https://curl.se/download.html)
 
+version=0.2.1
 cargo install --locked --no-default-features --features csharp --version 0.32.0 wit-bindgen-cli
-curl -OL https://github.com/WebAssembly/wasi-http/archive/refs/tags/v0.2.0.tar.gz
-tar xzf v0.2.0.tar.gz
-cp world.wit wasi-http-0.2.0/wit/world.wit
-wit-bindgen c-sharp -w wasi-http -r native-aot --internal --skip-support-files wasi-http-0.2.0/wit
-rm -r wasi-http-0.2.0 v0.2.0.tar.gz WasiHttpWorld_wasm_import_linkage_attribute.cs
+rm WasiHttpWorld*
+curl -OL https://github.com/WebAssembly/wasi-http/archive/refs/tags/v$version.tar.gz
+tar xzf v$version.tar.gz
+cp world.wit wasi-http-$version/wit/world.wit
+wit-bindgen c-sharp -w wasi-http -r native-aot --internal wasi-http-$version/wit
+rm -r wasi-http-$version v$version.tar.gz WasiHttpWorld_wasm_import_linkage_attribute.cs
